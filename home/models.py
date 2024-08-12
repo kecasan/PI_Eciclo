b59e10196765ee5b910f528922a8310af5a93195
Ana Iza Ribeiro <150141123+AnaIza13@users.noreply.github.com>
2024-08-12 11:35:05 -0300

    carrinho dando certo

--- home/models.py
+++ home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from datetime import datetime
+from django.utils.safestring import mark_safe
 
 # Create your models here.
+
--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from home import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("carrinho/", views.carrinho_view, name='carrinho'),
 ]
